78bbb468622aa78e4047707b2bff2185dfcdf2c0
altamiranoaxel77 <altamirano.programador@gmail.com>
2024-10-15 21:56:43 -0300

    inicio del CRUD usuario

---
+++ SistemaGestorDeVentas/api/user/UserDao.cs
@@ -0,0 +1,66 @@
+﻿using SistemaGestorDeVentas.db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestorDeVentas.api.user
+{
+    internal class UserDao
+    {
+        public Usuario createUserDao(Usuario nuevoUsuario)
+        {
+            using (var context = new sistema_de_ventas_Entities())
+            {
+                var usuario = context.Usuario.Add(nuevoUsuario);
+                context.SaveChanges();
+                return usuario;
+            }
+        }
+
+        public Usuario getUserDao(string dni)
+        {
+            using (var context = new sistema_de_ventas_Entities())
+            {
+                var usuario = context.Usuario.Find(dni);
+                return usuario;
+            }
+        }
+
+        public List<Usuario> getUsersDao()
+        {
+            using (var context = new sistema_de_ventas_Entities())
+            {
+                return context.Usuario.ToList();
+            }
+        }
+
+        public Usuario updateUserDao(Usuario userActualizado)
+        {
+            using (var context = new sistema_de_ventas_Entities())
+            {
+                var user = context.Usuario.Find(userActualizado.DNI_usuario);
+                if(user != null)
+                {
+                    user.email = userActualizado.email;
+                    user.id_estado = userActualizado.id_estado;
+                    user.nombre = userActualizado.nombre;
+                    user.pass = userActualizado.pass;
+                    user.id_rol = userActualizado.id_rol;
+
+
+                }
+        }
+
+        public Usuario deleteUserDao(string dni)
+        {
+            using (var context = new sistema_de_ventas_Entities())
+            {
+            }
+        }
+
+
+    }
+
+}
---
+++ SistemaGestorDeVentas/api/user/UserService.cs
@@ -0,0 +1,43 @@
+﻿using SistemaGestorDeVentas.db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaGestorDeVentas.api.user;
+
+namespace SistemaGestorDeVentas.api.user
+{
+    internal class UserService
+    {
+        UserDao userDao = new UserDao();
+        public Usuario createUser(Usuario user){
+            var usuario = userDao.createUserDao(user);
+            return usuario;
+        }
+
+        public Usuario getUser(string dni)
+        {
+            var user = userDao.getUserDao(dni);
+            return user;
+        }
+
+        public List<Usuario> getUsers()
+        {
+            var users = userDao.getUsersDao();
+            return users;
+        }
+
+        public Usuario updateUser(string dni)
+        {
+            var userUpdate = userDao.updateUserDao(dni);
+            return userUpdate;
+        }
+
+        public Usuario deleteUser(Usuario user)
+        {
+            var userDelete = userDao.deleteUserDao(user);
+            return userDelete;
+        }
+    }
+}
