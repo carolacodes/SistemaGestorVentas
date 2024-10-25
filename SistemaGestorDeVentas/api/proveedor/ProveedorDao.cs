713ab9c0249d4fe60098fe041a5fef90b92e259e
carolacodes <carolacardozog23@gmail.com>
2024-10-24 23:50:54 -0300

    Finalizacion archivos proveedorServices proveedorDao

--- SistemaGestorDeVentas/api/proveedor/ProveedorDao.cs
+++ SistemaGestorDeVentas/api/proveedor/ProveedorDao.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,7 @@ namespace SistemaGestorDeVentas.api.proveedor
 {
     internal class ProveedorDao
     {
-        public Proveedor CreateProveedorDao(Proveedor nuevoProveedor)
+        public Proveedor createProveedorDao(Proveedor nuevoProveedor)
         {
             try
             {
@@ -24,6 +25,91 @@ namespace SistemaGestorDeVentas.api.proveedor
             catch (Exception ex)
             {
                 throw new Exception("Error al intentar crear un nuevo proveedor: "+ ex.Message);
+            }
+        }
+
+       public Proveedor getProveedorDao(string codigo)
+        {
+            try
+            {
+                using (var context = new sistema_de_ventas_Entities())
+                {
+                    var proveedor = context.Proveedor.Find(codigo);
+                    return proveedor;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al intentar obtener un proveedor: " + ex.Message);
+            }
+        }
+
+        public Proveedor updateProveedorDao(Proveedor proveedorActualizado)
+        {
+            try
+            {
+                using (var context = new sistema_de_ventas_Entities())
+                {
+                    var proveedorExistente = context.Proveedor.Find(proveedorActualizado.id_proveedor);
+                    if (proveedorExistente != null)
+                    {
+                        proveedorExistente.email = proveedorActualizado.email;
+                        proveedorExistente.telefono = proveedorActualizado.telefono;
+                        proveedorExistente.direccion = proveedorActualizado.direccion;
+                        proveedorExistente.web = proveedorActualizado.web;
+                        proveedorExistente.id_estado = proveedorActualizado.id_estado;
+                        context.SaveChanges();
+                        return proveedorExistente;
+                    }
+                    // agregar un mensaje que diga que no se encuentra el cliente
+                    return proveedorExistente;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("error al actualizar al cliente: " + ex.Message);
+            }
+        }
+
+        public List<Proveedor> getProveedoresDao()
+        {
+
+            try
+            {
+                using (var context = new sistema_de_ventas_Entities())
+                {
+                    return context.Proveedor.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al intentar obtener todos los proveedores: " + ex.Message);
+            }
+        }
+
+        public Proveedor deleteProveedor(string codigo)
+        {
+            try
+            {
+                using (var context = new sistema_de_ventas_Entities())
+                {
+
+                    var proveedor = context.Proveedor.Find(codigo);
+                    if (proveedor != null)
+                    {
+                        proveedor.id_estado = 0;
+                        // 0 = 'inactivo'
+                        context.SaveChanges();
+                        return proveedor;
+                    }
+                    // Agregar mensaje de no se encontro cliente
+                    return proveedor;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al intentar eliminar el proveedor: " + ex.Message);
             }
         }
     }
--- SistemaGestorDeVentas/api/proveedor/ProveedorServices.cs
+++ SistemaGestorDeVentas/api/proveedor/ProveedorServices.cs
@@ -3,10 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SistemaGestorDeVentas.db;
 
 namespace SistemaGestorDeVentas.api.proveedor
 {
     internal class ProveedorServices
     {
+        ProveedorDao proveedorDao = new ProveedorDao();
+
+        public Proveedor createProveedor(Proveedor nuevoProveedor)
+        {
+            try
+            {
+                var proveedor = proveedorDao.createProveedorDao(nuevoProveedor);
+                return proveedor;
+            } catch (Exception ex)
+            {
+                throw new Exception("Error al crear un nuevo proveedor: " + ex.Message);
+            }
+        }
+
+        public Proveedor updateProveedor(Proveedor proveedorActualizado)
+        {
+            try
+            {
+                var proveedor = proveedorDao.updateProveedorDao(proveedorActualizado);
+                return proveedor;
+            }catch(Exception ex) 
+            { 
+                throw new Exception("Erro al intentar modificar los datos del proveedor" + ex.Message); 
+            }
+        }
+
+        public Proveedor deleteProveedor(string codigo)
+        {
+            try
+            {
+                var proveedorEliminado = proveedorDao.deleteProveedor(codigo);
+                return proveedorEliminado;
+            } catch(Exception ex)
+            {
+                throw new Exception("Error al intentar eliminar el proveedor: " + ex.Message);
+            }
+        }
+
+        public Proveedor getProveedor(string codigo)
+        {
+            try
+            {
+                var proveedor = proveedorDao.getProveedorDao(codigo);
+                return proveedor;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error al intentar obtener el proveedor: " + ex.Message);
+            }
+        }
+
+        public List<Proveedor> getProveedores()
+        {
+            try
+            {
+                List<Proveedor> proveedores = proveedorDao.getProveedoresDao();
+                return proveedores;
+            } catch (Exception ex)
+            {
+                throw new Exception("Error al intenat obtener todos los proveedores: " + ex.Message);
+            }
+        }
     }
 }
